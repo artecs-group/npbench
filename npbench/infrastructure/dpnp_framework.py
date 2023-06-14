765780e6ed6b898160b60304baa5d1ff0618f2ca
FRYoussef <f.r.youssef@hotmail.com>
2023-06-14 15:38:34 +0200

    fixed some issues

--- npbench/infrastructure/dpnp_framework.py
+++ npbench/infrastructure/dpnp_framework.py
@@ -1,5 +1,6 @@
 # Copyright 2021 ETH Zurich and the NPBench authors. All rights reserved.
 import pkg_resources
+import dpctl
 
 from npbench.infrastructure import Benchmark, Framework
 from typing import Any, Callable, Dict
@@ -14,6 +15,8 @@ class DpnpFramework(Framework):
         """
 
         super().__init__(fname)
+        d = dpctl.select_cpu_device()
+        d.print_device_info()
 
     def version(self) -> str:
         """ Return the framework version. """
@@ -24,3 +27,28 @@ class DpnpFramework(Framework):
         for copying the benchmark arguments. """
         import dpnp
         return dpnp.asarray
+
+    def setup_str(self, bench: Benchmark, impl: Callable = None) -> str:
+        """ Generates the setup-string that should be used before calling
+        the benchmark implementation.
+        :param bench: A benchmark.
+        :param impl: A benchmark implementation.
+        :returns: The corresponding setup-string.
+        """
+
+        if len(bench.info["array_args"]):
+            arg_str = self.out_arg_str(bench, impl)
+            copy_args = ["__npb_copy({})".format(a) for a in bench.info["array_args"]]
+            return arg_str + " = " + ", ".join(copy_args) + ";"
+        return ""
+
+    def exec_str(self, bench: Benchmark, impl: Callable = None):
+        """ Generates the execution-string that should be used to call
+        the benchmark implementation.
+        :param bench: A benchmark.
+        :param impl: A benchmark implementation.
+        """
+
+        arg_str = self.arg_str(bench, impl)
+        main_exec_str = "__npb_result = __npb_impl({a})".format(a=arg_str)
+        return main_exec_str + ";"
